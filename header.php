8afa9cd3421d6277a74e575d35c98a08401827cc
Nguyễn Trọng Đại <daitechno@outlook.com>
2023-06-29 15:08:06 +0700

    commit

---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Công ty TNHH Dịch Vụ Công Nghệ CDB Tech</title>
+  <meta content="Cung cấp dịch vụ IT, Máy chủ, Hệ thống mạng, Thiết kế Website" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="asets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Regna
+  * Updated: May 30 2023 with Bootstrap v5.3.0
+  * Template URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+ <!-- ======= Header ======= -->
+ <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div id="logo">
+        <a href="https://cdb-tech.com"><img src="assets/img/CDB-LOGO.png" alt=""></a>
+        <!-- Uncomment below if you prefer to use a text logo -->
+        <!--<h1><a href="index.html">Regna</a></h1>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Trang chủ</a></li>
+          <li><a class="nav-link scrollto" href="#about">Giới thiệu</a></li>
+          <li><a class="nav-link scrollto" href="#services">Dịch vụ</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Dự án</a></li>
+          <li><a class="nav-link scrollto" href="#partner">Đối tác</a></li>          
+          <li><a class="nav-link scrollto" href="#contact">Liên hệ</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
---
+++ it-support.php
@@ -0,0 +1,91 @@
+<?php include('header.php'); ?>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Dịch vụ IT Support Outsource</h1>
+      <h2>Cung cấp nhân lực IT Support, hoạt động như một nhân viên IT thực thụ</h2>
+    </div>
+  </section><!-- End Hero Section -->
+  <main id="main">
+
+    <!-- ======= Breadcrumbs Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Dịch vụ IT Support Outsource</h2>
+        </div>
+
+      </div>
+    </section><!-- Breadcrumbs Section -->
+
+    <!-- ======= Portfolio Details Section ======= -->
+    <section id="portfolio-details" class="portfolio-details">
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-8">
+            <div class="portfolio-details-slider swiper">
+              <div class="swiper-wrapper align-items-center">
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/portfolio-details-1.jpg" alt="">
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/portfolio-details-2.jpg" alt="">
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/portfolio-details-3.jpg" alt="">
+                </div>
+
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Bảng giá dịch vụ:</h3>
+              <ul>
+                <li><strong>Đơn giá</strong>: 80.000-100.000 / máy tính</li>
+                <li><strong>Nhân sự tại chỗ(Onsite)</strong>: Cung cấp nhân viên IT trực tại công ty khách hàng</li>
+                <li><strong>Nhân sự Online (Remote)</strong>: Cung cấp đội ngũ nhân viên IT trực tuyến, hỗ trợ qua các nền tảng remote</li>
+                <li><strong>Nhân sự bán Onsite (Remote + Onsite)</strong>: Cung cấp đội ngũ nhân viên IT trực tuyến, tại chỗ theo lịch định kỳ</li>
+                <li><strong>Thời gian tiếp nhận</strong>: Không quá 5 phút</li>
+                <li><strong>Thời gian xử lý sự cố</strong>: 15-45 phút với xử lý remote, 1h nội thành HCM với xử lý onsite</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-12">
+          <div class="portfolio-description">
+              <h2>Thông tin chi tiết</h2>
+              <p>Gói dịch vụ IT OUTSOURCE phù hợp với các doanh nghiệp vừa và nhỏ hoặc các văn phòng cần chuyên viên IT quản trị và vận hành.</p>
+              <ul>
+                <li>
+                  <h3>Nội dung công việc</h3>
+                </li>
+                <li>
+                  <h3>Đối với hệ thống máy chủ</h3>
+                </li>
+                <li>
+                  <h3>Đối với hệ thống máy trạm và người dùng</h3>
+                </li>
+                <li>
+                  <h3>Bảo trì định kỳ</h3>
+                </li>
+                
+              </ul>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Details Section -->
+
+  </main><!-- End #main -->
+<?php  include('footer.php'); ?>
