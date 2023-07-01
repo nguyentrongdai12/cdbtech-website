9c3eecb5ad46b15e97f3968c004c49863ed27091
Nguyễn Trọng Đại <daitechno@outlook.com>
2023-07-02 01:17:13 +0700

    get page all time

--- admin/index.php
+++ admin/index.php
@@ -2,12 +2,23 @@
     include_once("../controllers/dbclass.php");
     if (isset($_COOKIE["login"]))
     {        
-        include "header.php";
-        include "sidebar.php"; 
-        include "topbar.php";
-        include "dashboard.php";
-        include "footer.php";
-    }
+        if (isset($_GET['pages'])) {
+            $page = $_GET['pages'].".php";
+            include "header.php";
+            include "sidebar.php"; 
+            include "topbar.php";
+            include $page;
+            include "footer.php";
+        }
+        else 
+        {
+            include "header.php";
+            include "sidebar.php"; 
+            include "topbar.php";
+            include "dashboard.php";
+            include "footer.php";
+        }        
+    }     
     else {
         header("Location: login.php");
     }
