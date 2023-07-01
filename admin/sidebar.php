4ecfa41848bf622875b01cd40bcf46bbbdcd97d6
Nguyễn Trọng Đại <daitechno@outlook.com>
2023-07-02 01:42:05 +0700

    create menu in sidebar, go to sleep !!!

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -18,6 +18,68 @@
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
 </li>
+
+<!-- Nav Item - Menu Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsemenu"
+        aria-expanded="true" aria-controls="collapsePages">
+        <i class="fas fa-fw fa-bars"></i>
+        <span>Menu</span>
+    </a>
+    <div id="collapsemenu" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Menu:</h6>
+            <a class="collapse-item" href="index.php?pages=menu">Tất cả menu</a>
+        </div>
+    </div>
+</li>
+
+<!-- Nav Item - Services Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseservices"
+        aria-expanded="true" aria-controls="collapsePages">
+        <i class="fas fa-leaf"></i>
+        <span>Dịch vụ</span>
+    </a>
+    <div id="collapseservices" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Dịch vụ:</h6>
+            <a class="collapse-item" href="index.php?pages=services">Tất cả dịch vụ</a>
+        </div>
+    </div>
+</li>
+
+<!-- Nav Item - Project Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseproject"
+        aria-expanded="true" aria-controls="collapsePages">
+        <i class="fab fa-telegram-plane"></i>
+        <span>Dự án</span>
+    </a>
+    <div id="collapseproject" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Dự án, công việc, giải pháp:</h6>
+            <a class="collapse-item" href="index.php?pages=all_project">Tất cả dự án</a>
+            <a class="collapse-item" href="index.php?pages=category_project">Phân loại dự án</a>
+        </div>
+    </div>
+</li>
+
+<!-- Nav Item - Customer Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#accordioncustomers"
+        aria-expanded="true" aria-controls="collapsePages">
+        <i class="fas fa-user-alt"></i>
+        <span>Đối tác - Khách hàng</span>
+    </a>
+    <div id="accordioncustomers" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Đối tác - Khách hàng:</h6>
+            <a class="collapse-item" href="index.php?pages=all_project">Tất cả đối tác - khách hàng</a>
+        </div>
+    </div>
+</li>
+
 <!-- Sidebar Toggler (Sidebar) -->
 <div class="text-center d-none d-md-inline">
     <button class="rounded-circle border-0" id="sidebarToggle"></button>
